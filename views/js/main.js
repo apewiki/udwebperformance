d898c30f0f78179b7500bbf2485a10e624e1618f
ape101 <qzhujw@gmail.com>
2015-03-04 10:29:58 -0500

    Add backface-visibility: hidden to style.css instead of add it to each elem in generating moving pizza

--- views/js/main.js
+++ views/js/main.js
@@ -536,7 +536,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
-    elem.style.backfaceVisibility = "hidden";
+    //elem.style.backfaceVisibility = "hidden";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     elemPizza.appendChild(elem);
